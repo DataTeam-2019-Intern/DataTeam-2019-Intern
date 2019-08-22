284b0b9b42aa01e00a319658d08235a673cce7e6
Haydar128 <54399103+Haydar128@users.noreply.github.com>
2019-08-22 14:22:46 +0300

    Add files via upload

---
+++ Promise.js
@@ -0,0 +1,16 @@
+new Promise(function(resolve,reject){
+
+    setTimeout(() => {
+        
+        resolve(5);
+    }, 1000);
+
+}).then(function(number){
+    console.log(number);
+    return number*number;
+}).then(function(number){
+    console.log(number);
+    return number*number;
+}).then(function(number){
+    console.log(number);
+});

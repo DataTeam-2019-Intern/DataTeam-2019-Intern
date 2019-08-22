3f1c95a124cb8489de20f95ce9904b57f88f820a
Haydar128 <54399103+Haydar128@users.noreply.github.com>
2019-08-22 13:41:20 +0300

    Add files via upload

---
+++ Asenkron.js
@@ -0,0 +1,4 @@
+setTimeout(function yuzMilisaniyeSonra(){ console.log('1'); }, 100);
+setImmediate(function birSonrakiFonksiyon(){ console.log('2'); });
+setImmediate(function birSonrakiFonksiyon(){ console.log('4'); });
+console.log('başla!');

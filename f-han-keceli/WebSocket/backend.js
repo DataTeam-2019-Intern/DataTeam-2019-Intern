dcfa43c98c684d367e5c5890ca60c08d66436d99
Han Keceli(古関伴助) <31503875+hankeceli@users.noreply.github.com>
2019-09-02 16:20:59 +0300

    Update backend.js

--- f-han-keceli/WebSocket/backend.js
+++ f-han-keceli/WebSocket/backend.js
@@ -5,10 +5,10 @@ const server = http.createServer((request, response) => {
      response.end("...");
 });
 server.listen(1234);
-//1234 portuna gelecek olan tüm istekleri socket tarafından dinlememiz gerekmektedir.
+// We need to listen all requests which will come from 1234 port via socket. 
 const io = socket.listen(server);
 
 io.sockets.on("connection", socket => {
-    //Server'a bir client bağlandığı zaman "connection" isimli olay tetiklenecektir. socket parametresi ise bağlantı yapan client'ın bilgisini içermektedir.
-    console.log("User connection", socket.client.id);
+    // Connection will be triggered when client has been connected to the server. Socket parameter included client infos who is connected to the server.
+ console.log("User connection", socket.client.id);
 });
